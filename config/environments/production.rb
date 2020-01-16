dcfc625371c4114364f4a2ad0aafbaa1a48a6118
Kevin Bieri <bierike@gmail.com>
2020-01-16 21:59:03 +0100

    Rename from maestro to libellum

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "maestro_production"
+  # config.active_job.queue_name_prefix = "libellum_production"
 
   config.action_mailer.perform_caching = false
 
