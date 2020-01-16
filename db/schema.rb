3b4968aae4454ef73c31736b3a2335e3c40daae9
Kevin Bieri <bierike@gmail.com>
2020-01-16 21:55:23 +0100

    Adds default user and organisation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_081833) do
+ActiveRecord::Schema.define(version: 2020_01_16_204043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,10 @@ ActiveRecord::Schema.define(version: 2019_12_23_081833) do
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "firstname"
+    t.string "lastname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+organization = Organization.create!(handle: 'putztrix', name: 'Putztrix')
+organization.users.create!(
+  email: 'info@putztrix.ch',
+  password: 'bschorle',
+  admin: true,
+  first_name: 'Beatrix',
+  last_name: 'Bieri',
+  confirmed_at: Time.now
+)
