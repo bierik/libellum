a9ef5c3a1f7b53675d88a14cbb1a6b85d7dca21c
Yves Siegrist <Elektron1c97@gmail.com>
2020-01-21 18:37:47 +0100

    Extract rubocop test into command which is ran on CI
    The output is more readable if there's wrongly formatted.

--- test/linters/rubocop_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RubocopTest < ActiveSupport::TestCase
-  test 'everything formatted according to rubocop' do
-    assert_match(/no\ offenses\ detected/, `rubocop`)
-  end
-end
