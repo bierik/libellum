aceff886505b670b646752a0f8d1049f9700be4a
Yves Siegrist <Elektron1c97@gmail.com>
2020-02-07 19:06:15 +0100

    Fix creating a customer
    - Don't calculate route flat when creating a customer before validation
    - Only link "berechnen" when a customer is already created. Otherwise building the URL will fail

--- test/system/customers_test.rb
+++ test/system/customers_test.rb
@@ -16,4 +16,22 @@ class CustomersTest < ApplicationSystemTestCase
     click_on 'berechnen'
     assert_equal '5', find_field('Wegpauschale').value
   end
+
+  test 'create a customer' do
+    visit new_customer_path
+    click_on 'Kunde erstellen'
+
+    assert_equal 'muss ausgefüllt werden', all('.invalid-feedback').first.text
+
+    fill_in 'Vorname', with: 'Hans'
+    fill_in 'Nachname', with: 'Meier'
+    fill_in 'Strasse', with: 'Kleeweg'
+    fill_in 'Nummer', with: '12'
+    fill_in 'PLZ', with: '8888'
+    fill_in 'Ort', with: 'Baden'
+
+    click_on 'Kunde erstellen'
+
+    assert_equal "Kunde wurde erfolgreich erstellt.\n×", find('.alert').text
+  end
 end
