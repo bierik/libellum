d0154f0d75b6a6716c2ff6b935f3a37ee229cab7
Yves Siegrist <Elektron1c97@gmail.com>
2020-02-21 23:37:17 +0100

    Untie schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_143620) do
+ActiveRecord::Schema.define(version: 2020_02_21_223447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_143620) do
 
   create_table "organizations", force: :cascade do |t|
     t.string "handle", null: false
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "company"
@@ -147,8 +147,6 @@ ActiveRecord::Schema.define(version: 2020_02_04_143620) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "firstname"
-    t.string "lastname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
